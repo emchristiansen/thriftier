e84a00278bcdf4ad97dd083177e061461c2e60b1
Eric Martin Christiansen <ericmartinchristiansen@gmail.com>
2014-01-24 13:23:53 -0800

    stuff

--- bin/Thriftier.hs
+++ bin/Thriftier.hs
@@ -11,35 +11,46 @@ import System.Directory
 import Thriftier.Generate
 import Thriftier.Language
 
-thriftCommand :: FilePath -> FilePath -> String
-thriftCommand interfaceRoot thriftFile =
-  let directory = takeDirectory thriftFile in
+thriftCommand :: FilePath -> FilePath -> FilePath -> String
+thriftCommand interfaceRoot thriftFile outputDirectory =
+  {-let directory = takeDirectory thriftFile in-}
   printf 
-    "cd %s; thrift -I . --gen cpp:include_prefix -out %s %s" 
+    "cd %s; thrift -I %s --gen cpp:include_prefix -out %s %s" 
     interfaceRoot 
-    directory 
-    thriftFile  
+    interfaceRoot
+    outputDirectory 
+    (joinPath [interfaceRoot, thriftFile]) 
 
-runThrift :: FilePath -> FilePath -> IO ()
-runThrift interfaceRoot thriftFile = do
-  let command = thriftCommand interfaceRoot thriftFile
+runThrift :: FilePath -> FilePath -> FilePath -> IO ()
+runThrift interfaceRoot outputRoot thriftFile = do
+  let outputDirectory = takeDirectory $ joinPath [outputRoot, thriftFile]
+  createDirectoryIfMissing True outputDirectory 
+  let command = thriftCommand interfaceRoot thriftFile outputDirectory
   putStrLn command
   system command
   return ()
 
 main :: IO ()
 main = do
-  [interfaceRoot] <- getArgs
-  thriftierPaths <- find always (fileName ~~? "*.thriftier") interfaceRoot
-  mapM_ (writeThriftierToThrift interfaceRoot) thriftierPaths
+  [interfaceRoot, outputRoot] <- getArgs
+  {-thriftierPaths <- find always (fileName ~~? "*.thriftier") interfaceRoot-}
+  {-putStrLn $ show thriftierPaths-}
+  interfaceDirectories <- find always (fileType ==? Directory) interfaceRoot
+  putStrLn $ show interfaceDirectories
+  {-let -}
+    {-makeOutputDirectory interfaceDirectory = do-}
+      {-let outputDirectory = joinPath [outputRoot, interfaceDirectory]-}
+      {-createDirectoryIfMissing True outputDirectory-}
+  {-mapM makeOutputDirectory interfaceDirectories-}
+  {-mapM_ (writeThriftierToThrift interfaceRoot) thriftierPaths-}
   thriftPaths <- find always (fileName ~~? "*.thrift") interfaceRoot
   putStrLn $ show thriftPaths
-  mapM_ (runThrift interfaceRoot) thriftPaths
-  skeletonPaths <- find always (fileName ~~? "*_server.skeleton.cpp") interfaceRoot
-  putStrLn $ show skeletonPaths
-  mapM_ (generateHandler interfaceRoot) skeletonPaths
-  -- Remove the skeleton files.
-  mapM_ removeFile skeletonPaths
+  mapM_ (runThrift interfaceRoot outputRoot) thriftPaths
+  {-skeletonPaths <- find always (fileName ~~? "*_server.skeleton.cpp") outputRoot-}
+  {-putStrLn $ show skeletonPaths-}
+  {-mapM_ (generateHandler outputRoot) skeletonPaths-}
+   {-Remove the skeleton files.-}
+  {-mapM_ (\path -> removeFile $ joinPath [outputRoot, path]) skeletonPaths-}
   putStrLn "Done"
 
 
